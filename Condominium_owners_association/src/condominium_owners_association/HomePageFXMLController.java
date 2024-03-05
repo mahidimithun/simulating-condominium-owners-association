b5999f369dda3572f0ad64ab1a1d0beaca6b5186
MAHIDI MITHUN <144348728+mahidimithun@users.noreply.github.com>
2024-03-05 11:32:05 +0600

    Add files via upload
    log in

--- Condominium_owners_association/src/condominium_owners_association/HomePageFXMLController.java
+++ Condominium_owners_association/src/condominium_owners_association/HomePageFXMLController.java
@@ -9,7 +9,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,16 +22,26 @@ public class HomePageFXMLController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField tf_user_id;
+    @FXML
+    private PasswordField pw_user_pass;
+    @FXML
+    private ComboBox<?> cb_user_type;
+    
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void userLogIn(ActionEvent event) {
+    }
+
+    @FXML
+    private void userRegistration(ActionEvent event) {
+    }
     
 }
