0ddd41e83bc5a022bcd0d7852c4c41037c4220a5
Monosij kanti sarker <87165148+Monosijkantisarker-1234@users.noreply.github.com>
2024-03-05 12:59:35 +0600

    Add files via upload
    Security officer

---
+++ simulating-condominium-owners-association-main/Condominium_owners_association/src/condominium_owners_association/SCO_MainPageController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package condominium_owners_association;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class SCO_MainPageController implements Initializable {
+
+    @FXML
+    private TextField TF_visitorName;
+    @FXML
+    private TextField TF_Adress;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void VisitorOnclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void RemoveOnclick(ActionEvent event) {
+    }
+    
+}
